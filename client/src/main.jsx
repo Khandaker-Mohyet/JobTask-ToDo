c372a894c56cf6bca68b344161075d6bd0772a70
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-15 02:51:25 +0600

    again create all route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,30 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import {
+//   RouterProvider,
+// } from "react-router-dom";
+// import router from './routes/Router';
+
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <RouterProvider router={router} />
+//   </StrictMode>,
+// )
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div className='text-green-600'>Hello world!</div>,
-  },
-]);
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
+
