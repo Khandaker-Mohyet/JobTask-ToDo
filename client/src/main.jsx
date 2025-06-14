cc658d33ac5672cfd6cc723f325fa16db901d623
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-15 02:17:45 +0600

    create new client folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className='text-green-600'>Hello world!</div>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
