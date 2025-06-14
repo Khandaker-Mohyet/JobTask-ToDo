5162542c19bfbaf674d41caa3ec8aa3ffb6924fa
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-14 21:50:41 +0600

    connected mongoose

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv"
 
+import{connectDB} from "./src/lib/db.js"
 import authRoutes from "./src/routes/auth.route.js"
 
+dotenv.config()
 const app = express()
+
+const PORT = process.env.PORT
 
 app.use("/api/auth",authRoutes)
 
-app.listen(5005,()=>{
-    console.log("server is running on port 5005")
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+    connectDB()
 })
