02bc6cc5b5a982ecd0a4e39b59c7856223a3683c
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-15 17:03:43 +0600

    sittings page doen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,11 +9,13 @@ import { useAuthStore } from "./store/useAuthStore.js"
 import { useEffect } from "react"
 import {Loader} from "lucide-react"
 import { Toaster } from "react-hot-toast"
+import { useThemeStore } from "./store/useThemeStore.js"
 
 
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+  const {theme} = useThemeStore()
 
   useEffect(()=>{
     checkAuth()
@@ -28,15 +30,15 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={authUser?<HomePage/>:<Navigate to="login"/>}/>
+        <Route path="/" element={authUser?<HomePage/>:<Navigate to="/login"/>}/>
         <Route path="/signup" element={!authUser?<SignUpPage/>: <Navigate to="/"/>} />
         <Route path="/login" element={!authUser?<LoginPage/>: <Navigate to="/"/>} />
         <Route path="/settings" element={<SettingsPage/>} />
-        <Route path="/profile" element={authUser?<ProfilePage/>: <Navigate to="login"/>} />
+        <Route path="/profile" element={authUser?<ProfilePage/>: <Navigate to="/login"/>} />
           
       </Routes>
 
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
+import { Eye, EyeOff, Loader2, Lock, Mail, FileCheck2 } from "lucide-react";
 import { useAuthStore } from "../Store/useAuthStore";
 
 const LoginPage = () => {
@@ -28,7 +28,7 @@ const LoginPage = () => {
                 className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20
               transition-colors"
               >
-                <MessageSquare className="w-6 h-6 text-primary" />
+                <FileCheck2 className="w-6 h-6 text-primary" />
               </div>
               <h1 className="text-2xl font-bold mt-2">Welcome Back</h1>
               <p className="text-base-content/60">Sign in to your account</p>
