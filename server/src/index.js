4cf32d8c136cca1875b518c37a7e70aaeacbd90c
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-17 19:36:13 +0600

    try to solved provlem

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
@@ -26,13 +26,13 @@ app.use(cors({
 app.use("/api/auth",authRoutes)
 app.use("/api/tasks", taskRoutes);
 
-// if(process.env.NODE_ENV==="production"){
-//     app.use(express.static(path.join(__dirname,"../client/dist")));
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../client/dist")));
 
-//     app.get("*",(req,res)=>{
-//         res.sendFile(path.join(__dirname,"../client","dist","index.html"));
-//     })
-// }
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../client","dist","index.html"));
+    })
+}
 
 
 
